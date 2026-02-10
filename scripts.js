c268a6d5b53f377807b014f5bc1fb3f0c5e3347c
Julio Cezar <juliocezarrc16@gmail.com>
2026-02-10 11:37:12 -0300

    format the value to brazilian currency.

---
+++ scripts.js
@@ -0,0 +1,22 @@
+
+const amount = document.getElementById("amount")
+
+amount.oninput = function () {
+    let value = amount.value.replace(/\D/g, "")
+
+    value = Number(value) / 100
+
+    amount.value = formatCurrencyBRL(value)
+
+}
+
+function formatCurrencyBRL(value) {
+    value = value.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    })
+
+    return value
+}
+
+
